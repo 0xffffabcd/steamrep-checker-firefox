d6bacb0c89bb45c9ad8e30d3783a93a4cd5e8282
Nasreddine <snake.inside@gmail.com>
2015-05-07 22:31:53 +0200

    Add-on now has preferences.
    + Refactoring
    + Updated jquery (2.1.1 -> 2.1.3)
    + Updated jQueryUI  (1.11.0 -> 1.11.4)
    + Added a preference pane
    + bumped the addon version to 0.6

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ SteamRep checker
 ================
 
 This extension provides a quick way to check the reputation of a Steam user on SteamRep.com then provides visual
-feedback accordingly
+feedback accordingly.
--- data/checker.js
+++ data/checker.js
@@ -1,14 +1,92 @@
 /**
- * Crate a warning dialog in case the Steam user is a known scammer
+ * Object that holds the current addon preferences
+ *
+ * @type {{prefBackpack: (*|.contentScriptOptions.prefBackpack), prefDotaBP: (*|.contentScriptOptions.prefDotaBP), prefSteamgifts: (*|.contentScriptOptions.prefSteamgifts), prefCSGOValue: (.contentScriptOptions.prefCSGOvalue|*), prefGoogle: (*|.contentScriptOptions.prefGoogle)}}
+ */
+var Settings = {
+    prefBackpack: self.options.prefBackpack,
+    prefDotaBP: self.options.prefDotaBP,
+    prefSteamgifts: self.options.prefSteamgifts,
+    prefCSGOValue: self.options.prefCSGOvalue,
+    prefGoogle: self.options.prefGoogle
+};
+
+// Update values to reflect preferences changes while the addon is attached to a page
+self.port.on("prefBackpack", function (newValue) {
+    Settings.prefBackpack = newValue;
+});
+self.port.on("prefDotaBP", function (newValue) {
+    Settings.prefDotaBP = newValue;
+});
+self.port.on("prefSteamgifts", function (newValue) {
+    Settings.prefSteamgifts = newValue;
+});
+self.port.on("prefCSGOvalue", function (newValue) {
+    Settings.prefCSGOValue = newValue;
+});
+self.port.on("prefGoogle", function (newValue) {
+    Settings.prefGoogle = newValue;
+});
+
+/**
+ * Steam user object
+ *
+ * @type {{SteamID64: string, VacBanned: string, TradeBanState: string, IsLimitedAccount: string, Privacy: string}}
+ */
+var User = {
+    SteamID64: '',
+    VacBanned: '',
+    TradeBanState: '',
+    IsLimitedAccount: '',
+    Privacy: ''
+};
+
+/**
+ * SteamRep response object
+ *
+ * @type {{SteamID64: string, Reputation: string}}
+ */
+var SteamRepInfo = {
+    SteamID64: '',
+    Reputation: ''
+};
+
+/**
+ * Icons
+ *
+ * Shield Icons by paomedia (https://github.com/paomedia/small-n-flat)
+ * licensed under CC0 which is available here: https://github.com/paomedia/small-n-flat/blob/master/LICENSE
+ *
+ * @type {{Loading: string, ShieldGreen: string, ShieldYellow: string, ShieldRed: string, ShieldRedBig: string, faviconTF2Bp: string, faviconDota2bp: string, faviconSteamgifts: string, faviconCsgoValue: string, faviconGoogle: string, faviconSteamrep: string}}
+ */
+var Icons = {
+    Loading: '<image alt="loading" height="16" width="16" class="loading" src="' + self.options.LoadingIcon + '" />',
+
+    ShieldGreen: '<image alt="trusted" height="24" width="24" src="' + self.options.ShieldGreenIcon + '" />',
+    ShieldYellow: '<image alt="caution" height="24" width="24" src="' + self.options.ShieldYellowSmall + '" />',
+    ShieldRed: '<image alt="scammer" height="24" width="24" src="' + self.options.ShieldRedIcon + '" />',
+    ShieldRedBig: '<image alt="scammer" height="128" width="128" src="' + self.options.ShieldRedIconBig + '" style="float:left;" />',
+
+    faviconTF2Bp: '<image alt="Backpack.tf" height="16" width="16" src="' + self.options.faviconTF2Bp + '" />',
+    faviconDota2bp: '<image alt="D2.backpack.tf" height="16" width="16" src="' + self.options.faviconDota2bp + '" />',
+    faviconSteamgifts: '<image alt="Steamgifts.com" height="16" width="16" src="' + self.options.faviconSteamgifts + '" />',
+    faviconCsgoValue: '<image alt="CsgoValue" height="16" width="16" src="' + self.options.faviconCsgoValue + '" />',
+    faviconGoogle: '<image alt="Google" height="16" width="16" src="' + self.options.faviconGoogle + '" />',
+
+    faviconSteamrep: '<image alt="Steamrep" height="16" width="16" src="' + self.options.faviconSteamRep + '" class="src_icon" />'
+};
+
+
+/**
+ * Create a warning dialog in case the Steam user is a known scammer
  */
 function createScammerWarningDialog() {
-    var scammer_big_icon = '<img alt="scammer" width="128" height="128"  style="float:left;" src="'+ self.options.shieldRedBig +'" />';
 
     var dialog =
         '<div id="dialog-message" title="WARNING: SCAMMER">'
-            + '<p>' + scammer_big_icon + ' This user has been marked as a scammer on SteamRep.com. </p>'
-            + '<p>To protect yourself and prevent thieves from profiting, <b style="color: red;">do not trade with this person</b>.'
-            + 'Players shouldn\'t be encouraged to steal. Supporting them can hurt your reputation.</p>'
+        + '<p>' + Icons.ShieldRedBig + ' This user has been marked as a scammer on SteamRep.com. </p>'
+        + '<p>To protect yourself and prevent thieves from profiting, <b style="color: red;">do not trade with this person</b>.'
+        + 'Players shouldn\'t be encouraged to steal. Supporting them can hurt your reputation.</p>'
         + '</div>';
 
     $('body').prepend(dialog);
@@ -33,13 +111,6 @@ function createScammerWarningDialog() {
  */
 function tagUser(tagType, reputation) {
 
-    // Shield Icons
-    // by paomedia (https://github.com/paomedia/small-n-flat)
-    // licensed under CC0 which is available here: https://github.com/paomedia/small-n-flat/blob/master/LICENSE
-    var caution_icon = '<img alt="caution" width="24" height="24" src="'+ self.options.shieldYellowSmall +'" />';
-    var trusted_icon = '<img alt="trusted" width="24" height="24"  src="'+ self.options.shieldGreenSmall +'" />';
-    var scammer_icon = '<img alt="scammer" width="24" height="24"  src="'+ self.options.shieldRedSmall +'" />';
-
     switch (tagType) {
         case 'scammer':
             $('#src_rep').html(reputation);
@@ -47,7 +118,7 @@ function tagUser(tagType, reputation) {
 
             $('.playerAvatar.profile_header_size').css('background', 'red');
             $('div.profile_header_summary > div.persona_name').addClass('scammer');
-            $('div.profile_header_summary > div.persona_name').prepend(scammer_icon);
+            $('div.profile_header_summary > div.persona_name').prepend(Icons.ShieldRed);
 
             break;
         case 'caution':
@@ -56,7 +127,7 @@ function tagUser(tagType, reputation) {
 
             $('.playerAvatar.profile_header_size').css('background', 'orange');
             $('div.profile_header_summary > div.persona_name').addClass('caution');
-            $('div.profile_header_summary > div.persona_name').prepend(caution_icon);
+            $('div.profile_header_summary > div.persona_name').prepend(Icons.ShieldYellow);
 
             break;
         case 'trusted':
@@ -65,45 +136,41 @@ function tagUser(tagType, reputation) {
 
             $('.playerAvatar.profile_header_size').css('background', 'lime');
             $('div.profile_header_summary > div.persona_name').addClass('trusted');
-            $('div.profile_header_summary > div.persona_name').prepend(trusted_icon);
+            $('div.profile_header_summary > div.persona_name').prepend(Icons.ShieldGreen);
 
             break;
         default:
-			$('#src_rep').text('No special rep (there might be pending reports against this user)');
-			break;
+            $('#src_rep').text('No special rep (there might be pending reports against this user)');
+            break;
     }
 }
 
 /**
  * Parses the Steamrep response
- * @param reputation Steamrep response
- * @param steamID64 SteamID64 of the user
  */
-function handleRep(reputation, steamID64) {
-    if (reputation == '') {
-        tagUser('', reputation);
-		findPendingReports(steamID64);
+function handleRep() {
+    if (SteamRepInfo.Reputation == '') {
+        tagUser('', SteamRepInfo.Reputation);
+        findPendingReports(User.SteamID64);
     }
-    else if (reputation.search(/(banned|scammer)/i) > -1) {
-        tagUser('scammer', reputation);
+    else if (SteamRepInfo.Reputation.search(/(banned|scammer)/i) > -1) {
+        tagUser('scammer', SteamRepInfo.Reputation);
         createScammerWarningDialog();
     }
-    else if (reputation.search(/(admin|middleman|valve employee|trusted)/i) > -1) {
-        tagUser('trusted', reputation);
+    else if (SteamRepInfo.Reputation.search(/(admin|middleman|valve employee|trusted)/i) > -1) {
+        tagUser('trusted', SteamRepInfo.Reputation);
 
     }
-    else if (reputation.search(/caution/i) > -1) {
-        tagUser('caution', reputation)
+    else if (SteamRepInfo.Reputation.search(/caution/i) > -1) {
+        tagUser('caution', SteamRepInfo.Reputation)
     }
 }
 
 /**
  * Queries SteamRep.com for the reputation of a user
- *
- * @param steamID64 SteamID64 of the user
  */
-function querySteamRep(steamID64) {
-    var sr_api_url = 'https://steamrep.com/api/beta/reputation/' + steamID64 + '?json=1&source=sr-check';
+function querySteamRep() {
+    var sr_api_url = 'https://steamrep.com/api/beta/reputation/' + User.SteamID64 + '?json=1&source=sr-check';
 
     $.ajax({
         url: sr_api_url,
@@ -112,12 +179,15 @@ function querySteamRep(steamID64) {
         .done(function (xml, textStatus, jqxhr) {
             srResponse = jqxhr.responseJSON;
 
-            $('#src_rep').attr('title', srResponse.steamrep.reputation);
+            SteamRepInfo.Reputation = srResponse.steamrep.reputation;
+            SteamRepInfo.SteamID64 = User.SteamID64;
 
-            handleRep(srResponse.steamrep.reputation, steamID64);
+            $('#src_rep').attr('title', SteamRepInfo.Reputation);
+
+            handleRep(SteamRepInfo.Reputation, User.SteamID64);
 
         })
-        .fail(function (e) {
+        .fail(function () {
             $('#src_rep').text('Error communicating with SteamRep.com. Click here to visit the website.');
         });
 }
@@ -135,89 +205,91 @@ function findPendingReports(steamID64) {
         dataType: 'json'
     })
         .done(function (xml, textStatus, jqxhr) {
-			
-			srResponse = jqxhr.responseJSON;
-			
-			if (typeof(srResponse.status) !== 'undefined'){
-				if((srResponse.status == 'ok') && (srResponse.message == 'No results found.'))
-				{
-					$('#src_rep').attr('title', "No special rep (0 pending reports)");
-					$('#src_rep').text('No special rep (0 pending reports)');
-				} 
-			} else {			
-				$('#src_rep').attr('title', "There might be pending reports against this user");
-				$('#src_rep').text('No special rep (there might be pending reports against this user)');
-			}
+
+            srResponse = jqxhr.responseJSON;
+
+            if (typeof(srResponse.status) !== 'undefined') {
+                if ((srResponse.status == 'ok') && (srResponse.message == 'No results found.')) {
+                    $('#src_rep').attr('title', "No special rep (0 pending reports)");
+                    $('#src_rep').text('No special rep (0 pending reports)');
+                }
+            } else {
+                $('#src_rep').attr('title', "There might be pending reports against this user");
+                $('#src_rep').text('No special rep (there might be pending reports against this user)');
+            }
         })
 }
 
 /**
  * Display the the SteamID64, privacy level and adds links to 3rd party websites
  * to ease the background check
- *
- * @param steamID64 SteamID64 of the user
- * @param privacy privacy level of the profile
  */
-function displaySteamInfo(steamID64, privacy)
-{
-    $('#src_rep').attr('href', 'http://steamrep.com/profiles/' + steamID64);
+function displaySteamInfo() {
+    $('#src_rep').attr('href', 'http://steamrep.com/profiles/' + User.SteamID64);
 
-    switch (privacy){
+    var privacy = '';
+    switch (User.Privacy) {
         case 'public':
-            $('#steamrep_checker').append('<p><b>Profile privacy : </b>Public</p>');
+            privacy = 'Public';
             break;
         case 'friendsonly':
-            $('#steamrep_checker').append('<p><b>Profile privacy : </b>Friends Only</p>');
+            privacy = 'Friends Only';
             break;
         case 'usersonly':
-            $('#steamrep_checker').append('<p><b>Profile privacy : </b>Users Only</p>');
+            privacy = 'Users Only';
             break;
         case 'private':
-            $('#steamrep_checker').append('<p><b>Profile privacy : </b>Private</p>');
+            privacy = 'Private';
             break;
     }
 
-    $('#steamrep_checker').append('<p><b>Permalink : </b><a id="src_profile_permalink" href="http://steamcommunity.com/profiles/' + steamID64 + '">http://steamcommunity.com/profiles/' + steamID64 + '</a></p>');
-    $('#steamrep_checker').append('<p><b>Pending reports : </b><a id="src_pending_reports" href="http://forums.steamrep.com/search/search/?keywords=' + steamID64 + '&o=date">Search SteamRep Forum</a></p>');
-    $('#steamrep_checker').append('<p><label for="src_sid64tb">SteamID64 : </label><input id="src_sid64tb" type="text" value="' + steamID64 + '" readonly /></p>');
+    $('#steamrep_checker').append('<p><b>Profile privacy : </b>' + privacy + '</p>');
+
+    $('#steamrep_checker').append('<p><b>Permalink : </b><a id="src_profile_permalink" href="http://steamcommunity.com/profiles/' + User.SteamID64 + '">http://steamcommunity.com/profiles/' + User.SteamID64 + '</a></p>');
+    $('#steamrep_checker').append('<p><b>Pending reports : </b><a id="src_pending_reports" href="http://forums.steamrep.com/search/search/?keywords=' + User.SteamID64 + '&o=date">Search SteamRep Forums</a></p>');
+    $('#steamrep_checker').append('<p><label for="src_sid64tb">SteamID64 : </label><input id="src_sid64tb" type="text" value="' + User.SteamID64 + '" readonly /></p>');
 
 
-    // website icons
-    var tf2bp_icon = '<img alt="backpack.tf" width="16" height="16" class="src_icon" src="'+ self.options.backpackTF2 +'" />';
-    var dota2bp_icon = '<img alt="dota2.backpack.tf" width="16" height="16" class="src_icon" src="'+ self.options.backpackD2 +'" />';
-    var st_icon = '<img alt="steamtrades.com" width="16" height="16" class="src_icon" src="'+ self.options.steamTrades +'" />';
-    var csgovalue_icon = '<img alt="csgovalue.com" width="16" height="16" class="src_icon" src="'+ self.options.csgoValue +'" />';
-    var google_icon = '<img alt="google.com" width="16" height="16" class="src_icon" src="'+ self.options.google +'" />';
+    // External websites
+    $('#steamrep_checker').append('<p>');
+    if (Settings.prefBackpack)
+        $('#steamrep_checker').append('<a href="http://backpack.tf/profiles/' + User.SteamID64 + '">' + Icons.faviconTF2Bp + ' backpack.tf</a> | ');
+    if (Settings.prefDotaBP)
+        $('#steamrep_checker').append('<a href="http://dota2.backpack.tf/profiles/' + User.SteamID64 + '">' + Icons.faviconDota2bp + ' dota2.bp.tf</a> | ');
+    if (Settings.prefCSGOValue)
+        $('#steamrep_checker').append('<a href="http://www.csgovalue.com/?steamID=' + User.SteamID64 + '">' + Icons.faviconCsgoValue + ' csgovalue.com</a> | ');
+    if (Settings.prefSteamgifts)
+        $('#steamrep_checker').append('<a href="http://www.steamgifts.com/go/user/' + User.SteamID64 + '">' + Icons.faviconSteamgifts + ' Steamgifts.com</a> | ');
+    if (Settings.prefGoogle)
+        $('#steamrep_checker').append('<a href="https://www.google.com/search?q=' + User.SteamID64 + '">' + Icons.faviconGoogle + 'google.com</a></p>');
+    $('#steamrep_checker').append('</p>');
 
 
-    $('#steamrep_checker').append('<p>' + tf2bp_icon + '<a href="http://backpack.tf/profiles/' + steamID64 + '">backpack.tf</a> | '
-        + dota2bp_icon + '<a href="http://dota2.backpack.tf/profiles/' + steamID64 + '">dota2.bp.tf</a> | '
-        + csgovalue_icon + '<a href="http://www.csgovalue.com/?id=' + steamID64 + '">csgovalue.com</a> | '
-        + st_icon + '<a href="http://www.steamtrades.com/user/id/' + steamID64 + '">steamtrades.com</a> | '
-        + google_icon + '<a href="https://www.google.com/search?q=' + steamID64 + '">google.com</a></p>');
 }
 
 /**
  * retrieve the SteamID64 and the privacy level of a Steam profile
  */
-function getSteamInfo()
-{
-    var url = document.location.origin + document.location.pathname + '/?xml=1';
+function getSteamInfo() {
+    var url = document.location.href + '/?xml=1';
 
     $.ajax({
         url: url,
         dataType: 'xml'
     })
-    .done(function (xml) {
-        var steamID64 = $(xml).find("steamID64").text();
-        var privacy = $(xml).find("privacyState").text();
+        .done(function (xml) {
+            User.SteamID64 = $(xml).find("steamID64").text();
+            User.IsLimitedAccount = $(xml).find("isLimitedAccount").text();
+            User.TradeBanState = $(xml).find("tradeBanState").text();
+            User.VacBanned = $(xml).find("vacBanned").text();
+            User.Privacy = $(xml).find("privacyState").text();
 
-        displaySteamInfo(steamID64, privacy);
-        querySteamRep(steamID64);
-    })
-    .fail(function (e) {
-        $('#src_rep').text('Error getting the SteamID64');
-    });
+            displaySteamInfo();
+            querySteamRep(User.SteamID64);
+        })
+        .fail(function () {
+            $('#src_rep').text('Error getting the SteamID64');
+        });
 }
 
 /**
@@ -225,29 +297,29 @@ function getSteamInfo()
  *
  * @param title title of the infobox
  */
-function createInfoBox(title){
+function createInfoBox(title) {
 
     var infoBox =
         '<div id="src_profile_customization" class="profile_customization">'
-            + '    <div id="profile_customization_header"  class="profile_customization_header ellipsis">'
-            + '    ' + title
-            + '    </div>'
-            + '    <div class="profile_customization_block">'
-            + '        <div class="customtext_showcase">'
-            + '            <div class="showcase_content_bg showcase_notes">'
-            + '                <div id="steamrep_checker">'
-            + '                    <p>'
-            + '                        <img alt="steamrep.com" width="16" height="16" class="src_icon" src="'+ self.options.steamRep +'" />'
-            + '                        <b>Reputation : </b>'
-            + '                        <a href="#" id="src_rep">'
-            + '                            <img alt="loading" width="16" height="16" class="loading" src="'+ self.options.loading +'" /> Checking SteamRep...'
-            + '                        </a>'
-            + '                    </p>'
-            + '                </div>'
-            + '            </div>'
-            + '        </div>'
-            + '    </div>'
-            + '</div>';
+        + '    <div id="profile_customization_header"  class="profile_customization_header ellipsis">'
+        + '    ' + title
+        + '    </div>'
+        + '    <div class="profile_customization_block">'
+        + '        <div class="customtext_showcase">'
+        + '            <div class="showcase_content_bg showcase_notes">'
+        + '                <div id="steamrep_checker">'
+        + '                    <p>'
+        + '                        ' + Icons.faviconSteamrep
+        + '                        <b>Reputation : </b>'
+        + '                        <a href="#" id="src_rep">'
+        + '                            ' + Icons.Loading + ' Checking SteamRep...'
+        + '                        </a>'
+        + '                    </p>'
+        + '                </div>'
+        + '            </div>'
+        + '        </div>'
+        + '    </div>'
+        + '</div>';
 
     // Adding the profile info box
     if ($('.profile_customization_area').length > 0) {
@@ -260,10 +332,10 @@ function createInfoBox(title){
     }
 }
 
-var re = new RegExp("steamcommunity.com/(?:id|profiles)/[a-zA-Z0-9_]+[/]{0,1}$");
+var re = new RegExp("steamcommunity.com/(?:id|profiles)/[a-zA-Z0-9_-]+[/]+$");
 
 
-if (re.exec(document.location.origin + document.location.pathname)) {
-    createInfoBox('SteamRep Check report');
+if (re.exec(document.location.href)) {
+    createInfoBox('SteamRep Checker report');
     getSteamInfo();
 }
